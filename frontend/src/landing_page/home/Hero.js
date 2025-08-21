a0378951d49586d4283ab24463fcd9e008607343
Sajjad Ansari <132455347+sajjad6ansari@users.noreply.github.com>
2025-08-21 11:10:26 +0530

    Added Hero and Awards component in Home page (#4)
    Co-authored-by: sajjadansari313 <a.sajjad@unque.me>

--- frontend/src/landing_page/home/Hero.js
+++ frontend/src/landing_page/home/Hero.js
@@ -2,10 +2,17 @@ import React from 'react'
 
 const Hero = () => {
   return (
-    <h1>
-      Hero Section <i class="fa fa-user-circle" aria-hidden="true"></i>
-
-    </h1>
+    <div className='container p-5 mb-5'>
+      <div className="row text-center">
+        <img src="media/images/homeHero.png" alt="Hero section" className='mb-5' />
+        <h1 className='mt-5'>Invest in everything</h1>
+          <p>
+          Online platform to invest in stocks, derivatives, mutual funds, and more
+        </p> 
+        <button className='p-2 btn btn-primary fs-5 mb-5' style={{ width: "20%", margin: "0 auto" }}>Sign Up</button>
+      </div>
+      
+    </div>
     
   )
 }
