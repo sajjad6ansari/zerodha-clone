645726fe9ea12be7e485f3eb25b90a57eb077438
Sajjad Ansari <132455347+sajjad6ansari@users.noreply.github.com>
2025-08-21 14:55:10 +0530

    added react routing in the index.js (#7)
    Co-authored-by: sajjadansari313 <a.sajjad@unque.me>

--- frontend/src/landing_page/pricing/PricingPage.js
+++ frontend/src/landing_page/pricing/PricingPage.js
@@ -1,15 +1,11 @@
 import React from 'react'
 import Hero from './Hero'
 import Brokerage from './Brokerage'
-import Navbar from '../Navbar'
-import Footer from '../Footer'
 const PricingPage = () => {
   return (
     <>
-       <Navbar/> 
       <Hero/>
       <Brokerage/>
-      <Footer/>
     </>
   )
 }
--- frontend/src/landing_page/support/SupportPage.js
+++ frontend/src/landing_page/support/SupportPage.js
@@ -1,15 +1,11 @@
 import React from 'react'
-import Navbar from '../Navbar'
-import Footer from '../Footer'
 import Hero from './Hero'
 import CreateTicket from './CreateTicket'
 const SupportPage = () => {
   return (
     <>
-      <Navbar/>
       <Hero/>
       <CreateTicket/>
-      <Footer/>
     </>
   )
 }
